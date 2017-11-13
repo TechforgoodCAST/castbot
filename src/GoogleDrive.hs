e2e404a6866e9edc1f15d265d5655516746d8a9b
Andrew MacMurray <a.macmurray@icloud.com>
2017-11-13 17:57:20 +0000

    exit program with message if env vars are not set

--- src/GoogleDrive.hs
+++ src/GoogleDrive.hs
@@ -3,18 +3,20 @@
 module GoogleDrive where
 
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
 import Data.Aeson
-import Data.ByteString.Lazy   (ByteString)
-import Data.Foldable          (fold)
-import Data.Monoid            ((<>))
+import Data.ByteString.Lazy      (ByteString)
+import Data.Foldable             (fold)
+import Data.Monoid               ((<>))
 import Data.Text
-import GHC.Generics           (Generic)
+import GHC.Generics              (Generic)
 import GoogleDrive.OAuth
 import GoogleDrive.Types
 import Network.HTTP.Simple
-import Snap.Core              (Method (..), method, writeLBS)
+import Snap.Core                 (Method (..), method, writeLBS)
 import Snap.Snaplet
 import System.Environment
+import System.Exit               (exitFailure)
 
 sendToSlackHandler :: Handler b GoogleDrive ()
 sendToSlackHandler = method GET $ do
@@ -28,16 +30,22 @@ sendToSlack = do
   let req' = setRequestBodyJSON (SlackPost "hello from snap") req
   httpLBS req'
 
-getConfig :: MonadIO m => m Config
-getConfig = liftIO $ do
-  cId <- getEnv "CLIENT_ID"
-  cSc <- getEnv "CLIENT_SECRET"
-  let redirectUri = "http://localhost:8000/google-drive/redirect-auth"
+getConfig :: MonadIO m => m (Maybe Config)
+getConfig = liftIO . runMaybeT $ do
+  cId <- MaybeT $ lookupEnv "CLIENT_ID"
+  cSc <- MaybeT $ lookupEnv "CLIENT_SECRET"
   return $ Config (pack cId) (pack cSc) redirectUri
+  where
+    redirectUri = "http://localhost:8000/google-drive/redirect-auth"
+
+loadConfig :: MonadIO m => m Config
+loadConfig =
+  getConfig >>= maybe (liftIO $ putStrLn errMsg >> exitFailure) return
+  where errMsg = "please ensure CLIENT_ID & CLIENT_SECRET env vars are set"
 
 gDriveInit :: SnapletInit b GoogleDrive
 gDriveInit = makeSnaplet "google-drive" "google drive snaplet" Nothing $ do
-  config <- getConfig
+  config <- loadConfig
   addRoutes [ ("/boo", sendToSlackHandler)
             , ("/sign-in", method GET $ signInHandler config)
             , ("/redirect-auth", method GET $ redirectHandler config)
