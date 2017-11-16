edeeb6d14593a5efc3aa73bb5db6e9233e54d801
Andrew MacMurray <a.macmurray@icloud.com>
2017-11-16 21:11:05 +0000

    can check for files that have been created in the past minute

--- src/GoogleDrive/OAuth.hs
+++ src/GoogleDrive/OAuth.hs
@@ -2,21 +2,23 @@
 
 module GoogleDrive.OAuth where
 
-import           Control.Lens           (view)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.ByteString        (ByteString)
-import           Data.Maybe             (fromMaybe)
-import           Data.Monoid            ((<>))
-import qualified Data.Text              as T
-import           Data.Text.Encoding     (decodeUtf8, encodeUtf8)
-import           Database               (setTokens)
+import           Control.Lens              (view)
+import           Control.Monad.IO.Class    (liftIO)
+import           Data.ByteString.Char8     (ByteString, unpack)
+import           Data.DateTime
+import           Data.Maybe                (fromMaybe)
+import           Data.Monoid               ((<>))
+import qualified Data.Text                 as T
+import           Data.Text.Encoding        (decodeUtf8, encodeUtf8)
+import           Database                  (setTokens)
 import           GoogleDrive.Types
 import           Network.HTTP.Simple
-import qualified Network.HTTP.Simple    as HTTP
-import           Network.HTTP.Types.URI
+import qualified Network.HTTP.Simple       as HTTP
+import           Network.HTTP.Types.Header (hAuthorization)
+import           Network.HTTP.Types.URI    (renderQuery)
 import           Snap.Core
 import           Snap.Snaplet
-import           Snap.Snaplet.RedisDB   (runRedisDB)
+import           Snap.Snaplet.RedisDB      (runRedisDB)
 
 -- Handlers
 
@@ -27,7 +29,7 @@ redirectHandler :: Handler b GoogleDrive ()
 redirectHandler = do
   config <- view conf
   code   <- decode <$> getParam "code"
-  res    <- getResponseBody <$> (liftIO . requestAuthCredentials config $ AuthCode code)
+  res    <- liftIO . requestAuthCredentials config $ AuthCode code
   runRedisDB db $ setTokens res
   redirect "/google-drive/auth-success"
   where
@@ -38,53 +40,72 @@ signInHandler :: Handler b GoogleDrive ()
 signInHandler = view conf >>= (redirect . signInUrl)
 
 
+-- Files
+
+checkNewFiles :: Config -> RefreshToken -> IO Files
+checkNewFiles config rft = requestFilesInFolder config rft >>= newFiles
+
+newFiles :: Files -> IO Files
+newFiles (Files xs) = do
+  now <- getCurrentTime
+  return . Files $ filter (\x -> addMinutes 1 (createdDate x) > now) xs
+
+
 -- Requests
 
-requestAuthCredentials :: Config -> AuthCode -> IO (HTTP.Response AuthResponse)
+requestFilesInFolder :: Config -> RefreshToken -> IO Files
+requestFilesInFolder config refreshToken = do
+  tkn     <- encodeToken <$> requestAccessToken config refreshToken
+  baseReq <- parseRequest $ "GET https://www.googleapis.com/drive/v2/files" <> unpack filesQuery
+  let req = addRequestHeader hAuthorization ("Bearer " <> tkn) baseReq
+  getResponseBody <$> httpJSON req
+
+requestAuthCredentials :: Config -> AuthCode -> IO AuthResponse
 requestAuthCredentials config code = do
   baseReq <- parseRequest "POST https://www.googleapis.com/oauth2/v4/token"
   let formBody = authCredentialsFormBody config code
       req      = setRequestBodyURLEncoded formBody baseReq
-  httpJSON req
+  getResponseBody <$> httpJSON req
 
-requestAccessToken :: Config -> RefreshToken -> IO (HTTP.Response AccessToken)
+requestAccessToken :: Config -> RefreshToken -> IO AccessToken
 requestAccessToken config refreshToken = do
   baseReq <- parseRequest "POST https://www.googleapis.com/oauth2/v4/token"
   let formBody = accessTokenFormBody config refreshToken
       req      = setRequestBodyURLEncoded formBody baseReq
-  httpJSON req
+  getResponseBody <$> httpJSON req
 
 
 -- URI Helpers
 
+filesQuery :: ByteString
+filesQuery = renderQuery True
+  [ ("q", Just "\'1EMLrmlFkArl4txYQnMR62_gae4yjShux\' in parents and trashed = false") ]
+
 accessTokenFormBody :: Config -> RefreshToken -> [(ByteString, ByteString)]
-accessTokenFormBody config (RefreshToken x) =
-  [ ("client_id", enc $ clientId config)
-  , ("client_secret", enc $ clientSecret config)
-  , ("refresh_token", enc x)
+accessTokenFormBody config rft =
+  [ ("client_id",     clientId config)
+  , ("client_secret", clientSecret config)
+  , ("refresh_token", encodeToken rft)
   , ("grant_type", "refresh_token")
   ]
-  where enc = encodeUtf8
 
 authCredentialsFormBody :: Config -> AuthCode -> [(ByteString, ByteString)]
-authCredentialsFormBody config (AuthCode x) =
-  [ ("code",          enc x)
-  , ("client_id",     enc $ clientId config)
-  , ("client_secret", enc $ clientSecret config)
-  , ("redirect_uri",  enc $ redirectUri config)
+authCredentialsFormBody config ac =
+  [ ("code",          encodeToken ac)
+  , ("client_id",     clientId config)
+  , ("client_secret", clientSecret config)
+  , ("redirect_uri",  redirectUri config)
   , ("grant_type",    "authorization_code")
   ]
-  where enc = encodeUtf8
 
 signInUrl :: Config -> ByteString
 signInUrl config = baseUrl <> renderQuery True
-   [ ("client_id",     Just . enc $ clientId config)
-   , ("redirect_uri",  Just . enc $ redirectUri config)
-   , ("scope",         Just $ enc scope)
+   [ ("client_id",     Just $ clientId config)
+   , ("redirect_uri",  Just $ redirectUri config)
+   , ("scope",         Just scope)
    , ("access_type",   Just "offline")
    , ("response_type", Just "code")
   ]
   where
-    enc     = encodeUtf8
     baseUrl = "https://accounts.google.com/o/oauth2/v2/auth"
     scope   = "https://www.googleapis.com/auth/drive.readonly"
